02701b219cec461560099ad9408c8d259af15f46
yplazas <yeialex9511@gmail.com>
2025-02-10 23:40:28 -0500

    Initial commit: se crea la estructura del proyecto, usando tailwindCss

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <>
+    <div className="flex gap-3">
+      <p className="text-blue-500 text-2xl">hola</p>
+      <p className="text-blue-500 text-2xl">hola</p>
+      <p className="text-blue-500 text-2xl">hola</p>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
